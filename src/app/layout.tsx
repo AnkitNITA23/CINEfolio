466300f09d06b5e516a8edc31c776032f1f1f39b
Ankit Kumar <kdmspokharahan@gmail.com>
2025-12-16 10:43:39 +0000

    Hey I want the cinefolio logo used in Nabar to display in chrome tab in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎬</text></svg>"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
